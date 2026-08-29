539c590519ae65e77688059e4dc6268f441c43e9
ViniArcan <viniarcanfonseolive@gmail.com>
2026-08-29 21:26:20 +0000

    pensando...

--- teste.c
+++ teste.c
@@ -2,14 +2,13 @@
 #include <stdlib.h>
 #include <math.h>
 
-typedef struct POL {
-
+typedef struct POL { /* Usando uma estrutura de arvore para nossos Polinomios,
+                        assim podemos conseguir complexidade linear (na vdd é até um pouco melhor) e quadratica para o produto */
     bool monomio;
 
     long long int grau;
     int coef, tam;
 
-    POL *Seq;
+    POL *Par, *Impar;
 
-} POL;
-
+}

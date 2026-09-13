b43270f9b4433d833ac46a6c1df400e546f19d99
ViniArcan <viniarcanfonseolive@gmail.com>
2026-09-13 19:13:34 +0000

    Terminando as implementacoes

--- teste.c
+++ teste.c
@@ -3,30 +3,37 @@
 #include <math.h>
 #include "teste.h"
 
-struct POL {
-
+typedef struct mon {
     long long int grau;
     long long int coef;
-    struct POL *prox;
+    struct mon *prox;
+} MON;
 
+struct pol {
+    MON *inicio;
 };
 
-POL *Pol_criar(void){
+POL* criar_pol(void){
 
     POL *P = (POL*) malloc(sizeof(POL)); if (P == NULL){ return NULL; }
 
-    P->grau = 0;
-    P->coef = 0;
-    P->prox = NULL;
+    P->inicio = NULL;
 
     return P;
 
-} // Complexidade Const 🥴
+}
 
-void LIBERA(POL **P){
-    if((*P == NULL) || (P == NULL)){ return; }
+MON* criar_mon(long long int c, long long int g) {
+    MON *M = (MON*) malloc(sizeof(MON)); if (M == NULL) { return NULL; }
+    
+    M->coef = c; M->grau = g; M->prox = NULL;
+    return M;
+}
 
-    POL *Pos = P, *Aux;
+void LIBERA(POL *P){
+    if(P == NULL){ return; }
+
+    MON *Pos = P->inicio, *Aux;
 
     while(Pos != NULL){
         Aux = Pos->prox;
@@ -34,24 +41,38 @@ void LIBERA(POL **P){
         Pos = Aux;
     }
 
-    *P = NULL;
+    free(P);
     return;
 }
 
-void COPIA(POL **Q, POL *P){
+bool LIMPA(POL *P){
+    if(P == NULL){ return FALSE; }
 
-    if(Q == NULL){ return; }
+    MON *Pos = P->inicio, *Aux;
 
-    if(P == NULL){ LIBERA(Q); return; }
+    while(Pos != NULL){
+        Aux = Pos->prox;
+        free(Pos);
+        Pos = Aux;
+    }
 
-    LIBERA(Q); *Q = Pol_criar(); if(*Q == NULL){ return; }
+    P->inicio = NULL;
+    return TRUE;
+}
 
-    POL *PosP = P, *PosQ = *Q;
+bool COPIA(POL *Q, POL *P){
+
+    if( (Q == NULL) || (P == NULL) ){ LIMPA(Q); return FALSE; }
+
+    LIMPA(Q);
+
+    MON *PosP = P->inicio; if(PosP == NULL){ return TRUE; }
+    Q->inicio = criar_mon(PosP->coef, PosP->grau); if(Q->inicio == NULL){ return FALSE; }
+    PosP = PosP->prox;
+    MON *PosQ = Q->inicio;
 
     while(PosP != NULL){
-        POL *Aux = Pol_criar(); if(Aux == NULL){ LIBERA(Q); return; }
-        
-        Aux->coef = PosP->coef; Aux->grau = PosP->grau;
+        MON *Aux = criar_mon(PosP->coef, PosP->grau); if(Aux == NULL){ LIMPA(Q); return FALSE; }
 
         PosQ->prox = Aux;
         PosQ = Aux;
@@ -59,256 +80,239 @@ void COPIA(POL **Q, POL *P){
         PosP = PosP->prox;
     }
 
-    
-    return;
+    return TRUE;
 } // Complexidade O(N)
 
-void ADD(POL **P, long long int c, long long int g){
+bool ADD(POL *P, long long int c, long long int g){
+    
+    if( P == NULL ){ return FALSE; }
+    if( c == 0 ){ return TRUE; }
 
-    if( (c == 0) || (P == NULL)){
-        return;
+    if(P->inicio == NULL){
+        P->inicio = criar_mon(c, g);
+        return TRUE;
     }
 
-    POL *Pos = *P;
+    MON *Pos = P->inicio;
 
     if(g < Pos->grau){
-        POL *T = Pol_criar(); if(T == NULL){ return; }
-        T->coef = c; g; T->prox = Pos;
-        *P = T;
-        return;
+        MON *M = criar_mon(c, g); if(M == NULL){ return FALSE; }
+        M->prox = Pos; P->inicio = M;
+        return TRUE;
     }
     if(g == Pos->grau){
-        if(c == -(Pos->coef)){
-            *P = Pos->prox;
+        Pos->coef += c;
+        if(Pos->coef == 0){
+            P->inicio = Pos->prox;
             free(Pos);
-        }else{
-            Pos->coef += c;
         }
-        return;
+        return TRUE;
     }
 
-    while( ((Pos->prox) != NULL) && (g > (Pos->prox)->grau) ){
+    while( (Pos->prox != NULL) && (g > (Pos->prox)->grau) ){
         Pos = Pos->prox;
     }
 
-    if( ((Pos->prox) != NULL) && (g == (Pos->prox)->grau) ){
-
-        if(c == -((Pos->prox)->coef)){
-
-            POL *Aux = Pos->prox;
-            Pos->prox = (Pos->prox)->prox;
-            free(Aux);
-            return;
-
-        }
+    if( (Pos->prox != NULL) && (g == (Pos->prox)->grau) ){
 
         (Pos->prox)->coef += c;
-        return;
 
+        if((Pos->prox)->coef == 0){
+            MON *Aux = Pos->prox;
+            Pos->prox = (Pos->prox)->prox;
+            free(Aux);
+        }
+        return TRUE;
     }
 
-    POL *T = Pol_criar(); if(T == NULL){ return; }
-    
-    T->grau = g; T->coef = c; T->prox = Pos->prox;
-    Pos->prox = T;
-
-    return;
+    MON *M = criar_mon(c, g); if(M == NULL){ return FALSE; }
+    M->prox = Pos->prox;
+    Pos->prox = M;
+    return TRUE;
 } // Complexidade O(N)
 
 long long int COEF(POL *P, long long int g){
 
-    if(P == NULL){
-        return 0;
-    }
-    if(g == P->grau){
-        return P->coef;
-    }
-    return COEF(P->prox; g);
+    if(P == NULL){ return 0; }
 
+    MON *Pos = P->inicio; if(Pos == NULL){ return 0; }
+
+    while( (Pos != NULL) && (g > Pos->grau) ){
+        Pos = Pos->prox;
+    }
+    if((Pos != NULL) && (g == Pos->grau) ){
+        return Pos->coef;
+    }
+
+    return 0;
 }
 
-POL *REMOVE(POL *P, long long int g){
-    if(P == NULL){
+bool REMOVE(POL *P, long long int g){
+    if(P == NULL){ return FALSE; }
+    
+    MON *Pos = P->inicio; if(Pos == NULL){ return FALSE; }
+
+    if(g == Pos->grau){
+        P->inicio = Pos->prox;
+        free(Pos);
+        return TRUE;
+    }
+    while( (Pos->prox != NULL) && (g > (Pos->prox)->grau) ){
+        Pos = Pos->prox;
+    }
+    if((Pos->prox != NULL) && (g == (Pos->prox)->grau) ){
+        MON *Aux = (Pos->prox)->prox;
+        free(Pos->prox);
+        Pos->prox = Aux;
+        return TRUE;
+    }
+
+    return FALSE;
+}
+
+bool REMOVEMENOR(POL *P){
+    if(P == NULL){ return FALSE; }
+    if(P->inicio == NULL){ return FALSE; }
+    
+    MON *Aux = (P->inicio)->prox;
+    free(P->inicio);
+    P->inicio = Aux;
+    return TRUE;
+}
+
+long long int GRAU(POL *P){
+    if(P == NULL){ return -1; }
+    
+    MON *Pos = P->inicio; if(Pos == NULL){ return -1; }
+
+    while(Pos->prox != NULL){
+        Pos = Pos->prox;
+    }
+
+    return Pos->grau;
+}
+
+POL* SOMA(POL*P, POL *Q){
+    if( (P == NULL) || (Q == NULL) ){ return NULL; }
+
+    MON *PosP = P->inicio, *PosQ = Q->inicio;
+
+    POL *T = criar_pol(); if(T == NULL){ return NULL; }
+
+    if(PosP == NULL){
+        if(COPIA(T, Q)){
+            return T;
+        }
+        LIBERA(T);
         return NULL;
     }
-    if(g == P->grau){
-        POL Aux = *P;
-        free(P);
-        return Aux.prox;
-    }
-    P->prox = REMOVE(P->prox, g);
-    return P;
-}
-
-void REMOVEMENOR(POL *P){
-    if(P->prox == NULL){
-        P->coef = 0; P->grau = 0;
-        return;
-    }
-
-    P->coef = (P->prox)->coef; P->grau = (P->prox)->grau;
-    POL Aux = *(P->prox);
-    free(P->prox);
-    P->prox = Aux.prox;
-    return;
-}
-
-long long int MAIOR(POL *P){
-    if(P->prox == NULL){
-        return P->grau;
-    }
-    return MAIOR(P->prox);
-}
-
-// *R = SOMA( &&P, &&Q);
-POL *SOMA(POL *P, POL *Q){ 
-
-    POL *T; T = Pol_criar();
-
-    if (Q->grau > P->grau){
-
-        T->grau = P->grau; T->coef = P->coef;
-
-        if(P->prox == NULL){
-
-            COPIA(T->prox, Q);
-            return T;
-
-        }
-
-        T->prox = SOMA(P->prox, Q);
-        return T;
-    }
-
-    if (P->grau > Q->grau){
-
-        T->grau = Q->grau; T->coef = Q->coef;
-
-        if(Q->prox == NULL){
-
-            COPIA(T->prox, P);
-            return T;
-
-        }
-
-        T->prox = SOMA(P, Q->prox);
-        return T;
-    }
-
-    T->coef = P->coef + Q->coef; T->grau = P->grau;
-
-    if(P->prox == NULL){
-        if(Q->prox == NULL){
-            if(T->coef == 0){ return NULL; }
+    if(PosQ == NULL){
+        if(COPIA(T, P)){
             return T;
         }
-
-        if(T->coef == 0){
-            COPIA(T, Q->prox);
-        }else{ COPIA(T->prox, Q->prox); }
-
-        return T;
-    }
-    if(Q->prox == NULL){
-
-        if(T->coef == 0){
-            COPIA(T, P->prox);
-        }else{ COPIA(T->prox, P->prox); }
-
-        return T;
+        LIBERA(T);
+        return NULL;
     }
 
-    if(T->coef == 0){
-        free(T);
-        return SOMA(P->prox, Q->prox);
+    MON **PosT = &(T->inicio);
+
+    while((PosP != NULL) && (PosQ != NULL)){
+
+        if(PosP->grau < PosQ->grau){
+            *PosT = criar_mon(PosP->coef, PosP->grau); if(*PosT == NULL){ LIBERA(T); return NULL; }
+            PosT = &((*PosT)->prox);
+
+            PosP = PosP->prox;
+        }else if(PosQ->grau < PosP->grau){
+            *PosT = criar_mon(PosQ->coef, PosQ->grau); if(*PosT == NULL){ LIBERA(T); return NULL; }
+            PosT = &((*PosT)->prox);
+
+            PosQ = PosQ->prox;
+        }else{
+            long long int s = PosP->coef + PosQ->coef;
+
+            if(s != 0){
+                *PosT = criar_mon( s, PosP->grau); if(*PosT == NULL){ LIBERA(T); return NULL; }
+                PosT = &((*PosT)->prox);
+            }
+            
+            PosP = PosP->prox; PosQ = PosQ->prox;
+        }
     }
 
-    T->prox = SOMA(P->prox, Q->prox);
+    MON *R = (PosP != NULL) ? PosP : PosQ;
+    
+    while (R != NULL) {
+        *PosT = criar_mon(R->coef, R->grau); if(*PosT == NULL){ LIBERA(T); return NULL; }
+        PosT = &((*PosT)->prox);
+        R = R->prox;
+    }
+
     return T;
 } // Complexidade O(N + M) 😐
 
-void MonProd(*P, c, g){
-
-    if(P == NULL){ return; }
-
-    P->coef *= c; P->grau += g;
-    MonProd(P->prox, c, g);
-    return;
-    
-} // Complexidade O(N) 
 
 
 // R = PROD(&P, &Q); R = (p0 + Pp) * (q0 + Qp) = p0*q0 + q0*Pp + p0*Qp + Pp*Qp
-POL PROD(*P, *Q){
+POL* PROD(POL *P, POL *Q){
 
-    POL T0 = Pol_criar();
+    if( (P == NULL) || (Q == NULL) ){ return NULL; }
 
-    if(P->prox == NULL){
-        COPIA(&T0, Q);
-        MonProd(&T0, P->coef, P->grau);
-        return T0;
+    POL *T = criar_pol(); if(T == NULL){ return NULL; }
+
+    if( (P->inicio == NULL) || (Q->inicio == NULL) ){ return T; }
+
+    MON **PosT = &(T->inicio);
+
+    MON *PosP = P->inicio, *PosQ = Q->inicio;
+
+    while( (PosP != NULL) ){
+        *PosT = SOMA(MonProd(Q, PosP->coef, PosP->grau), T); if(PosT == NULL){ LIBERA(T); return NULL}
+        PosT = &((*PosT)->prox);
+        PosP = PosP->prox;
     }
-    if(Q->prox == NULL){
-        COPIA(&T0, P);
-        MonProd(T0, Q->coef, Q->grau);
-        return T0;
-    }
 
-    POL T1 = Pol_criar(), T2 = Pol_criar();
-
-    T0->coef = P->coef * Q->coef; T0->grau = P->grau + Q->grau;
-
-    COPIA(&T1, P->prox); COPIA(&T2, Q->prox);
-
-    MonProd(T1, Q->coef, Q->grau); MonProd(T2, P->coef, P->grau);
-
-    POL T01 = SOMA( &T1, &T2 ); T02 = PROD(P->prox, Q->prox);
-
-    LIBERA(&&T1); LIBERA(&&T2);
-
-    T0.prox = SOMA( &T01, &T02 );
-
-    LIBERA(&&T01); LIBERA(&&T02);
-
-    return T0;
-
+    return T;
 } //Complexidade é O(N^2 + M^2) 🫩
 
-void IMPRIME_AUX(POL *P){
+void IMPRIME_AUX(MON *M){
 
-    if(P == NULL){ return; }
+    if(M == NULL){ return; }
 
-    IMPRIME_AUX(P->prox);
-    printf("%lld*x^%lld ", (P->coef), (P->grau));
+    IMPRIME_AUX(M->prox);
+    printf("%lld*x^%lld ", (M->coef), (M->grau));
 
     return;
+}
+
+bool IMPRIME(POL *P){
+
+    if(P == NULL){ printf("-1\n"); return FALSE; }
+
+    MON *M = P->inicio; if(M == NULL){ printf("-1\n"); return TRUE; }
+
+    IMPRIME_AUX(M->prox);
+
+    printf("%lld*x^%lld\n", (M->coef), (M->grau));
+
+    return TRUE;
 
 }
 
-void IMPRIME(POL *P){
+bool IMPRIMEINV(POL *P){
 
-    IMPRIME_AUX(P->prox);
+    if(P == NULL){ printf("-1\n"); return FALSE; }
 
-    printf("%lld*x^%lld\n", (P->coef), (P->grau));
+    MON *Pos = P->inicio; if(Pos == NULL){ printf("-1\n"); return TRUE; }
 
-    return;
-
-}
-
-void IMPRIMEINV(POL *P){
-
-    if(P->prox == NULL){
-
-        printf("%lld*x^%lld\n", (P->coef), (P->grau));
-        
-        return;
+    while(Pos->prox != NULL){
+        printf("%lld*x^%lld ", (Pos->coef), (Pos->grau));
+        Pos = Pos->prox;
     }
 
-    printf("%lld*x^%lld ", (P->coef), (P->grau));
-    IMPRIME(P->prox);
+    printf("%lld*x^%lld\n", (Pos->coef), (Pos->grau));
 
-    return;
-
+    return TRUE;
 }
 
 
@@ -372,4 +376,107 @@ bool PROD(POL **P, POL **Q, POL *R){
     R.grau = P.grau; R.coef = P.coef + Q.coef;
     return TRUE;
 }
+*/
+
+/* *R = SOMA( &&P, &&Q);
+POL *SOMA(POL *P, POL *Q){ 
+
+    POL *T = Pol_criar();
+
+    if (Q->grau > P->grau){
+
+        T->grau = P->grau; T->coef = P->coef;
+
+        if(P->prox == NULL){
+
+            COPIA(T->prox, Q);
+            return T;
+
+        }
+
+        T->prox = SOMA(P->prox, Q);
+        return T;
+    }
+
+    if (P->grau > Q->grau){
+
+        T->grau = Q->grau; T->coef = Q->coef;
+
+        if(Q->prox == NULL){
+
+            COPIA(T->prox, P);
+            return T;
+
+        }
+
+        T->prox = SOMA(P, Q->prox);
+        return T;
+    }
+
+    T->coef = P->coef + Q->coef; T->grau = P->grau;
+
+    if(P->prox == NULL){
+        if(Q->prox == NULL){
+            if(T->coef == 0){ return NULL; }
+            return T;
+        }
+
+        if(T->coef == 0){
+            COPIA(T, Q->prox);
+        }else{ COPIA(T->prox, Q->prox); }
+
+        return T;
+    }
+    if(Q->prox == NULL){
+
+        if(T->coef == 0){
+            COPIA(T, P->prox);
+        }else{ COPIA(T->prox, P->prox); }
+
+        return T;
+    }
+
+    if(T->coef == 0){
+        free(T);
+        return SOMA(P->prox, Q->prox);
+    }
+
+    T->prox = SOMA(P->prox, Q->prox);
+    return T;
+} // Complexidade O(N + M) 😐
+*/
+
+/*
+bool APAGAMON(MON **M){
+    if((M == NULL) || (*M == NULL)){ return FALSE; }
+    
+    MON *Pos = *M, *Aux;
+    
+    while(Pos != NULL){
+        Aux = Pos->prox;
+        free(Pos);
+        Pos = Aux;
+    }
+
+    *M = NULL;
+    return TRUE;
+}
+
+MON* MonProd(MON *M, long long int c, long long int g){
+
+    if(M == NULL){ return NULL; }
+    
+    MON *T = NULL;
+    if(c == 0){ return NULL; }
+
+    MON *Pos = M;
+    MON **PosT = &(T);
+
+    while(Pos != NULL){
+        *PosT = criar_mon(Pos->coef * c, Pos->grau + g); if(*PosT == NULL){ APAGAMON(&T); return NULL; }
+        PosT = &((*PosT)->prox);
+        Pos = Pos->prox;
+    }
+    return T;
+} // Complexidade O(N)
 */

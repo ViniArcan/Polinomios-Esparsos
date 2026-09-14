061ad6fc083b7bc0696dd8aa6d9dbfec6a6d5a00
ViniArcan <viniarcanfonseolive@gmail.com>
2026-09-14 01:36:11 +0000

    working on main

--- main.c
+++ main.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 #include "pol_esp.h"
+
+#define MAX_POLS 512
+
+typedef struct {
+    POL *Pol;
+    char Nome[11];
+} PolNome;
 
 void DEF(char nome[], int k, int Tam, PolNome *Polinomios){
 
-    (*Polinomios)[Tam].Pol = criar_pol(); (*Polinomios)[Tam].Nome = nome;
+    Polinomios[Tam].Pol = criar_pol(); strcpy(Polinomios[Tam].Nome, nome);
+
+    POL *P = Polinomios[Tam].Pol;
 
     while(k>0){
         long long int c, g;
@@ -15,22 +25,46 @@ void DEF(char nome[], int k, int Tam, PolNome *Polinomios){
     }
 }
 
-typedef struct PolNome{
-    POL *Pol;
-    char Nome[];
-}
+
 
 bool main(){
 
-    PolNome *Polinomios[512] = (PolNome*) malloc(512 * sizeof(PolNome));
-    if (Polinomios == NULL){ printf("Falha na alocacao de memoria :(\n"); return false; }
+    PolNome *Polinomios[512] = (PolNome*) malloc(MAX_POLS * sizeof(PolNome));
+    if (Polinomios == NULL){ printf("Falha na alocacao de memoria :(\n"); return 1; }
 
     int Tam = 0;
+    char operacao[13];
 
     while(true){
 
 
 
+        if(scanf("%12s", comando) != 1){
+            break; 
+        }
+
+        if(strcmp(comando, "DEF") == 0){
+            char nome[32];
+            int k;
+            
+            scanf("%10s %d", nome, &k);
+            
+            if (Tam < MAX_POLS){
+                DEF(nome, k, Tam, Polinomios);
+                Tam++;
+            } else {
+                printf("Tem muito Polinimio po...\n");
+            }
+        }
+        else if(strcmp(comando, "FIM") == 0){
+            break;
+        }
+
+
+    }
+
+    for(int i=0; i<Tam; i++){
+        LIBERA(Polinomios[i].Pol);
     }
 
     free(Polinomios);
